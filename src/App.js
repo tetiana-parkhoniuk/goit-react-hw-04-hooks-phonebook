1e5a81e03a14b75e8b080a1e1193018d8886c4c2
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-07-28 17:27:29 +0300

    Rewrites with Hooks (useEffect) and Local storage.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import toast, { Toaster } from 'react-hot-toast';
 import ContactForm from 'components/ContactForm';
@@ -7,15 +7,22 @@ import ContactsList from 'components/ContactsList';
 import styles from 'App.module.css';
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ??
+      [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  });
   const [filter, setFilter] = useState('');
-
   const filterInputId = uuidv4();
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
 
   const addContact = ({ name, number }) => {
     const contact = {
